577a17a767cc2c1d8a48dccb206736dbbde60bf3
p40apartament <staszewskitm@gmail.com>
2025-07-31 19:47:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // ----------------------------------------
     // Płynne przewijanie do sekcji
+    // ----------------------------------------
     const navLinks = document.querySelectorAll('.main-nav a');
+    const menuToggle = document.querySelector('.menu-toggle');
+    const mainNav = document.querySelector('.main-nav');
+    const mainHeader = document.querySelector('.main-header');
 
-    for (const link of navLinks) {
-        link.addEventListener('click', smoothScroll);
+    if (navLinks) {
+        for (const link of navLinks) {
+            link.addEventListener('click', smoothScroll);
+        }
     }
 
     function smoothScroll(event) {
@@ -13,17 +20,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Zamknij menu po kliknięciu w link na urządzeniach mobilnych
         if (window.innerWidth <= 768) {
-            const menuToggle = document.querySelector('.menu-toggle');
-            const mainNav = document.querySelector('.main-nav');
-            const mainHeader = document.querySelector('.main-header');
-
             mainNav.classList.remove('active');
             menuToggle.classList.remove('active');
             mainHeader.classList.remove('menu-open');
         }
 
         if (targetSection) {
-            const headerHeight = document.querySelector('.main-header').offsetHeight;
+            // Upewnij się, że nagłówek istnieje, aby nie wystąpił błąd
+            const headerHeight = mainHeader ? mainHeader.offsetHeight : 0;
             window.scrollTo({
                 top: targetSection.offsetTop - headerHeight,
                 behavior: 'smooth'
@@ -31,11 +35,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // ----------------------------------------
     // Obsługa menu mobilnego
-    const menuToggle = document.querySelector('.menu-toggle');
-    const mainNav = document.querySelector('.main-nav');
-    const mainHeader = document.querySelector('.main-header');
-
+    // ----------------------------------------
     if (menuToggle && mainNav) {
         menuToggle.addEventListener('click', () => {
             mainNav.classList.toggle('active');
